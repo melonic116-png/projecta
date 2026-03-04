ca15354fa374a277dd4e283752b0910a3b80e29c
melonic116-png <melonic116@gmail.com>
2026-03-04 16:26:50 +0500

    Update app.py

--- app.py
+++ app.py
@@ -44,25 +44,12 @@ return {"status": "API работает"}
 
 @app.post("/save")
 def save_data(data: SaveRequest):
-
-```
 key = f"{data.user_id}_{data.group}"
-
 storage[key] = [item.dict() for item in data.items]
-
 save_storage(storage)
-
-return {
-    "status": "saved",
-    "items": len(data.items)
-}
-```
+return {"status": "saved", "items": len(data.items)}
 
 @app.get("/data")
 def get_data(user_id: int, group: str):
-
-```
 key = f"{user_id}_{group}"
-
 return storage.get(key, [])
-```
